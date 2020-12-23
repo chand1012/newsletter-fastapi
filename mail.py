026417b8888d0734577edd32fad21ab40faa25f8
chand1012 <chandler@chand1012.dev>
2020-12-22 21:57:03 -0500

    More work on email functions

--- mail.py
+++ mail.py
@@ -2,7 +2,15 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, From, Content
 
-def send_notification_email(subscriber_emails, subject, content):
+def send_notification_email(subscribers, subject, content):
+    if len(subscribers) == 0:
+        print('No recipients for the message, discarding.')
+        return
+    
+    subscriber_emails = []
+    for sub in subscribers:
+        subscriber_emails += [sub.get('key')]
+
     message = Mail(
         from_email=From(email=os.environ.get('SENDGRID_SEND_EMAIL'), name="chand1012.dev Blog"),
         to_emails=subscriber_emails,
@@ -13,7 +21,7 @@ def send_notification_email(subscriber_emails, subject, content):
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     resp = sg.send(message)
 
-    if resp.status_code != 200:
+    if resp.status_code >= 300 or resp.status_code < 200:
         print(resp.body)
         print(resp.headers)
 
@@ -25,10 +33,12 @@ def send_confirm_email(email, key):
         from_email=From(email=os.environ.get('SENDGRID_SEND_EMAIL'), name="chand1012.dev Blog"),
         to_emails=email,
         subject='chand1012.dev Subscription Confirmation',
-        html_content=Content("text/html", content.replace("{{key}}", key))
+        html_content=Content("text/html", content.replace("{{key}}", key).replace("{{url}}", os.environ.get('BASE_URL')))
     )
 
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     resp = sg.send(message)
-    print(resp.body)
-    print(resp.headers)
+    
+    if resp.status_code >= 300 or resp.status_code < 200:
+        print(resp.body)
+        print(resp.headers)
